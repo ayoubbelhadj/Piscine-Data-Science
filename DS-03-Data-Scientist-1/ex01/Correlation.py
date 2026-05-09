5f88facb20b7ff4084c0cd4e2b0b295d4403cc73
Ayoub Belhadj <aubbhj@gmail.com>
2026-05-09 10:56:47 +0100

    DS03-ex01: Correlation

---
+++ DS-03-Data-Scientist-1/ex01/Correlation.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def load(path: str) -> pd.DataFrame:
+    try:
+        df = pd.read_csv(path)
+        return df
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+if __name__ == "__main__":
+    df = load("../Train_knight.csv")
+    df["knight"] = df["knight"].map({"Sith":0, "Jedi":1})
+    correlations = df.corr()["knight"].abs().sort_values(ascending=False)
+    print(correlations.to_string())
